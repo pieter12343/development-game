ff2e306b9bc2ff82c4e8a9edcccef30880aab0da
pieter12343 <p.manders@sintlucasedu.nl>
2022-11-16 16:48:05 +0100

    movement and jumping(not finished)

--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -5,19 +5,39 @@ using UnityEngine;
 public class player_movement : MonoBehaviour
 {
     public float speed = 10;
-    private float jump = 0f;
-    private float jump_rate = 30f;
-    
+    public float jumpForce;
+    public float groundCheckLength;
+
+
+    private Rigidbody2D rb2D;
+
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         float dir = Input.GetAxis("Horizontal");
         transform.Translate(transform.right * dir * speed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.Y))
+        if (Input.GetButtonDown("Jump"))
         {
-            transform.position += transform.up * speed * Time.deltaTime;
+            rb2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
         }
-    }
 
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, groundCheckLength);
+        if (hit.collider != null)
+        {
+            Debug.Log("hit object = " + hit.collider.gameObject.name);
+        }
+        else
+        {
+            Debug.Log("hit nothing");
+        }
+
+        Debug.DrawRay(transform.position, -transform.up * groundCheckLength, Color.red);
+    }
 }
